a34bedcbb338202c60cc0af5a0176bc5cdafe655
Sonal <lanetteam.sonalmaniya@gmail.com>
2017-01-03 12:07:09 +0530

    added a temp variable

--- coredataDemo/coredataDemo/ViewController.swift
+++ coredataDemo/coredataDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     @IBOutlet var tvcTask: UITableView!
     var tasks : [Task] = []
     var editIndex: IndexPath!
+    var temp = 0 //added new variable
     
     override func viewDidLoad() {
         super.viewDidLoad()
