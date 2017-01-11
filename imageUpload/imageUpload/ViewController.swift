6c2a21da635ca538c386ff9de82b0e2624377f0c
Sonal <lanetteam.sonalmaniya@gmail.com>
2017-01-11 15:48:42 +0530

    apply run script using tailor

--- imageUpload/imageUpload/ViewController.swift
+++ imageUpload/imageUpload/ViewController.swift
@@ -8,14 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
-    
-    
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
     @IBOutlet var imgView: UIImageView!
     var imagePicker: UIImagePickerController!
     @IBOutlet var btnUpload: UIButton!
     @IBOutlet var lblError: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker = UIImagePickerController()
@@ -24,34 +23,32 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         imagePicker.allowsEditing = false
         btnUpload.isHidden = true
         lblError.text = ""
-        
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
     @IBAction func btnChooseImg(_ sender: UIButton) {
         self.present(imagePicker, animated: true, completion: nil)
     }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
         btnUpload.isHidden = false
         self.imgView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     @IBAction func btnUpload(_ sender: UIButton) {
         var categoryData = [String: String]()
         categoryData["categoryName"] = "Test from app"
         categoryData["description"] = "test"
-        var imageData = [String: UIImage]();
+        var imageData = [String: UIImage]()
         imageData["categoryImage1"] = imgView.image!
         imageData["categoryImage3"] = UIImage(named: "123.jpg")
-        
         server_API.sharedObject.requestFor_NSMutableDictionary(Str_Request_Url: "category", Request_parameter: categoryData, Request_parameter_Images: imageData, isTokenEmbeded: false, status: { (status) in
         }, response_Dictionary: { (result) in
-            if let message =  result.object(forKey: "message"){
+            if let message =  result.object(forKey: "message") {
                 DispatchQueue.main.async {
                     self.lblError.text = message as? String
                 }
@@ -59,6 +56,5 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
         })
         btnUpload.isHidden = true
     }
-    
-}
 
+}
